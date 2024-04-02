b201fe766c2eca0166fb06437742c29cf13b673e
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-04-02 18:47:59 -0500

    chore: configuration tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+import autoprefixer from 'autoprefixer';
+import postcss from 'postcss';
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	darkMode: 'class',
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,astro}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+				heading: [
+					'Inter',
+					'-apple-system',
+					'BlinkMacSystemFont',
+					'Segoe UI',
+					'Roboto',
+					'ui-sans-serif',
+					'system-ui',
+					'sans-serif',
+					'Apple Color Emoji',
+					'Segoe UI Emoji',
+					'Segoe UI Symbol',
+				],
+			},
+			fontWeight: {
+				normal: '400',
+				bold: '700',
+			},
+		},
+	},
+	plugins: [postcss, autoprefixer],
+};
