00feacafcfa23f4bacf54e5c7363b43b69bee2d1
Pedro Purihuaman <pedropuriihuaman@gmail.com>
2024-04-02 19:23:06 -0500

    refactor: improve error handling and response

---
+++ src/services/fetch-repositories.ts
@@ -0,0 +1,71 @@
+import type { IRepository } from '@interfaces/IRepository';
+import { CustomError } from './custom-error';
+
+const DEFAULT_BRANCH = 'start';
+
+const API_URL_BASE =
+	import.meta.env.API_URL_BASE ??
+	'https://api.github.com/users/puriihuaman/repos';
+
+export const fetchRepositories = async (
+	selectedTechnology: string
+): Promise<{ data: IRepository[]; error: CustomError | null }> => {
+	try {
+		const response = await fetch(API_URL_BASE);
+
+		if (!response.ok) {
+			if (response.status === 404) {
+				throw new CustomError(
+					'Invalid URL',
+					response.status,
+					'Could not access URL'
+				);
+			}
+
+			if (response.status === 403) {
+				throw new CustomError(
+					'Request limit',
+					response.status,
+					'Exceeded request limits'
+				);
+			}
+
+			throw new CustomError(
+				'GitHub API error',
+				response.status,
+				response.statusText
+			);
+		}
+
+		const repositories: IRepository[] = await response.json();
+
+		const filteredRepositories = repositories.filter(
+			(repository: IRepository): boolean =>
+				repository.default_branch === DEFAULT_BRANCH
+		);
+
+		const leakedRepositories = filteredRepositories.filter(
+			(repository: IRepository): boolean =>
+				repository.topics.includes(selectedTechnology)
+		);
+
+		const listOfFilteredRepositories =
+			leakedRepositories.length > 0 ? leakedRepositories : filteredRepositories;
+
+		return {
+			data: listOfFilteredRepositories || [],
+			error: null,
+		};
+	} catch (error) {
+		if (error instanceof CustomError) return { data: [], error };
+		else
+			return {
+				data: [],
+				error: {
+					name: 'Server error',
+					statusCode: 500,
+					message: 'Internal server error',
+				},
+			};
+	}
+};
